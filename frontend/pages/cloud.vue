d0cba96f09a29a87694db6c82d82cbb75c4bb50f
Andrey Chernikov <linuxoid.andrew@gmail.com>
2025-03-27 08:41:38 +0300

    Frontend minor fixes

--- frontend/pages/cloud.vue
+++ frontend/pages/cloud.vue
@@ -1,72 +1,64 @@
-<script>
+<script setup>
 import Datepicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
-import {ref, onMounted} from "vue";
+import {ref, onMounted, computed} from "vue";
 
 import BaseButton from "../components/BaseButton";
 import BaseCard from "../components/BaseCard";
 import WordCloud from "../components/WordCloud";
 import {NSpace, NDatePicker, NButton, NInputNumber} from "naive-ui";
+import {format} from 'date-fns';
 
-function transformWords(words){
-  var result = [];
+const date = ref([Date.now() - 10080, Date.now()]);
+const state = ref({limit: 100})
+const errors = ref([])
+const fetchedWords = ref([])
+
+const transformWords = (words) => {
+    var result = [];
   words.forEach(function (a) {
     result.push({name: a.word, value: a.count})
   })
   return result
 }
-export default {
-  name: "cloud",
-
-  components: {WordCloud, BaseCard, BaseButton, NSpace, NDatePicker, NButton, NInputNumber},
-  data() {
-    return {
-      date: [],
-      fetchedWords: [],
-      errors: [],
-      state: {
-        limit: 100
-      }
-    }
-  },
-  methods: {
-    retrieveWords() {
-      useMyFetch(
+const retrieveWords = () => {
+  const startDate = format(
+      new Date(date.value[0]), 'dd-MM-yyyy'
+  )
+  const endDate = format(
+      new Date(date.value[1]),
+      'dd-MM-yyyy'
+  )
+        useMyFetch(
           'dashboard/stats/',
           {
             params: {
-              date__gt: this.date[0], date__lt: this.date[1]
+              date__gt: startDate, date__lt: endDate, limit: state.value.limit
             }
           }).then(response => {
-        this.fetchedWords = transformWords(response.data.value);
+        fetchedWords.value = transformWords(response.data.value);
       })
           .catch(e => {
-            this.errors.push(e)
+            errors.value.push(e)
           })
-    },
-    increaseLimit() {
-      this.state.limit += 100
-      this.retrieveWords()
-    },
-  },
-
-
-  setup() {
-    const date = ref();
-
-    onMounted(() => {
-      const startDate = new Date();
+}
+const increaseLimit = () => {
+  state.value.limit += 100;
+  retrieveWords();
+}
+onMounted(() => {
+        const startDate = new Date();
       const endDate = new Date(new Date().setDate(startDate.getDate() + 7));
       date.value = [startDate, endDate]
-    })
-  },
-  computed: {
-    defaultDate() {
-      const startDate = new Date(new Date().setMonth(new Date().getMonth() - 1));
+})
+const defaultDate = computed(() => {
+        const startDate = new Date(new Date().setMonth(new Date().getMonth() - 1));
       const endDate = new Date();
       return [startDate, endDate];
-    }
-  }
+})
+function updateDate(value){
+  console.log(value)
+  date.value = value;
 }
 </script>
 
@@ -78,12 +70,14 @@ export default {
 <!--      <div class="row mb-2">-->
 <!--        <div class="w-25 mb-2">-->
           <NSpace>
-  <NDatePicker type="daterange" v-model="date" range :enable-time-picker="false" :default-value="defaultDate"/>
+  <NDatePicker
+      type="daterange" v-model:value="date" range :enable-time-picker="false" :default-value="defaultDate"
+  />
             <NInputNumber v-model:value="state.limit"/>
 <NButton mb-2 @click=retrieveWords()>Топ слов</NButton>
           </NSpace>
         <div class="row mb-2 mt-4" v-if="fetchedWords && fetchedWords.length">
-          <WordCloud :words="{words: fetchedWords}"></WordCloud>
+          <WordCloud :words="fetchedWords"></WordCloud>
 
 <!--          <BaseCard v-for="item in fetchedWords" :key="item.word" :title=item.word :text="item.count"/>-->
         </div>
